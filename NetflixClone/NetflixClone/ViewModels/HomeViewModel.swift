6a0a673f2e079faa38d845acc612286df0063981
GauravSemwal46 <gauravsemwal6@gmail.com>
2025-03-19 08:36:43 +0530

    create view modell for the home screen

--- NetflixClone/NetflixClone/ViewModels/HomeViewModel.swift
+++ NetflixClone/NetflixClone/ViewModels/HomeViewModel.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+class HomeViewModel: ObservableObject {
+    // string == category
+    @Published var movies: [String: [Movie]] = [:]
+    
+    public var allCategories: [String] {
+        return movies.keys.map({ String($0) })
+    }
+    
+    public func getMovie(forCategory category: String) -> [Movie] {
+        return movies[category] ?? []
+    }
+    
+    init() {
+        setupMovies()
+    }
+    
+    func setupMovies() {
+        movies["Trending Now"] = exampleMovies
+        movies["Stand-Up Comedy"] = exampleMovies.shuffled()
+        movies["New Release"] = exampleMovies.shuffled()
+        movies["Watch It Again"] = exampleMovies.shuffled()
+        movies["Sci"] = exampleMovies.shuffled()
+    }
+}
