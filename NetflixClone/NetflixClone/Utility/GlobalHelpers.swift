050b1eb52c7b8f6ec68d9b79adfc1ea2a46313f2
GauravSemwal46 <gauravsemwal6@gmail.com>
2025-03-19 08:37:17 +0530

    create helpers to get dummy movies

--- NetflixClone/NetflixClone/Utility/GlobalHelpers.swift
+++ NetflixClone/NetflixClone/Utility/GlobalHelpers.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+let exampleMovie1 =  Movie(id: UUID().uuidString, name: "Dark", thumbnailUrl: URL(string: "https://picsum.photos/200/300")!)
+let exampleMovie2 =  Movie(id: UUID().uuidString, name: "Travelers", thumbnailUrl: URL(string: "https://picsum.photos/200/301")!)
+let exampleMovie3 =  Movie(id: UUID().uuidString, name: "Community", thumbnailUrl: URL(string: "https://picsum.photos/200/302")!)
+let exampleMovie4 =  Movie(id: UUID().uuidString, name: "Alone", thumbnailUrl: URL(string: "https://picsum.photos/200/303")!)
+let exampleMovie5 =  Movie(id: UUID().uuidString, name: "Hannibal", thumbnailUrl: URL(string: "https://picsum.photos/200/304")!)
+let exampleMovie6 =  Movie(id: UUID().uuidString, name: "After life", thumbnailUrl: URL(string: "https://picsum.photos/200/305")!)
+
+let exampleMovies = [exampleMovie1, exampleMovie2, exampleMovie3, exampleMovie4, exampleMovie5, exampleMovie6]
