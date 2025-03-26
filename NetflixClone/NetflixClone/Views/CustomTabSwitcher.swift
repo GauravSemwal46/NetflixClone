935917595f44c23429324e4c78225e71f37be3ef
GauravSemwal46 <gauravsemwal6@gmail.com>
2025-03-26 09:47:28 +0530

    add functionality to select the custom tab

--- NetflixClone/NetflixClone/Views/CustomTabSwitcher.swift
+++ NetflixClone/NetflixClone/Views/CustomTabSwitcher.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct CustomTabSwitcher: View {
+    @State private var currentTab: CustomTab = .episodes
+    
     var tabs: [CustomTab]
     
     func widthForTab(_ tab: CustomTab) -> CGFloat {
@@ -24,20 +26,31 @@ struct CustomTabSwitcher: View {
                         VStack {
                             Rectangle()
                                 .frame(width: widthForTab(tab), height: 6)
+                                .foregroundColor(tab == currentTab ? .red : .clear)
                             Button {
-                                //
+                                currentTab = tab
                             } label: {
                                 Text(tab.rawValue)
                                     .font(.system(size: 16, weight: .bold))
+                                    .foregroundColor(tab == currentTab ? .white : .gray)
                             }
                             .buttonStyle(PlainButtonStyle())
+                            .frame(width: widthForTab(tab), height: 30)
                         }
                     }
                 }
             }
             
             // Selected View
-            Text("selected View")
+            switch currentTab {
+            case .episodes:
+                Text(currentTab.rawValue)
+            case .trailers:
+                Text(currentTab.rawValue)
+            case .more:
+                Text(currentTab.rawValue)
+            }
+            
         }
         .foregroundColor(.white)
     }
